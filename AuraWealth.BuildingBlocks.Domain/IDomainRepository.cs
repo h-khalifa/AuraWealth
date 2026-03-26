5e53c6d9534d8bef7f3abbca2406d4c3bf2de910
h-khalifa <haitham_khalifa@outlook.com>
2026-03-26 10:37:31 +0200

    adding IDomainRepository

---
+++ AuraWealth.BuildingBlocks.Domain/IDomainRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace AuraWealth.BuildingBlocks.Domain
+{
+    public interface IDomainRepository<TEntity> where TEntity : IAggregateRoot
+    {
+        Task<TEntity?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+        Task AddAsync(TEntity item, CancellationToken cancellationToken = default);
+        Task DeleteAsync(TEntity item, CancellationToken cancellationToken = default);
+    }
+}
